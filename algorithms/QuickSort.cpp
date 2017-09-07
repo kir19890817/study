fbb224c0bf34c7a1c9d17c66dac0319b437e6327
Kir <kondrashovks1989@gmail.com>
2017-09-07 07:30:09 +0200

    Quick sort

--- algorithms/QuickSort.cpp
+++ algorithms/QuickSort.cpp
@@ -1,8 +1,34 @@
 #include "Driver.h"
 
+int findPivot(std::vector<int>& input, int begin, int end)
+{
+  int pivot = input[begin];
+  int i = begin - 1;
+  int j = end + 1;
+  while(true) {
+    do ++i; while (input[i] < pivot);
+    do --j; while(input[j] > pivot);
+    if (i >= j) {
+      return j;
+    }
+    std::swap(input[begin], input[end]);
+  }
+}
+
+void doSort(std::vector<int>& input, int begin, int end)
+{
+  if (begin < end) {
+    int pivot = findPivot(input, begin, end);
+    doSort(input, begin, pivot);
+    doSort(input, pivot + 1, end);
+  }
+};
+
 void quickSort(std::vector<int>& input)
 {
+  doSort(input, 0, input.size() - 1);
 }
+
 
 int main(int argn, char* argv[]) 
 {

7cc03c2e0d91996911b68ca71d2ce6fa040c5ae3
Kirill Kondraschoff <KirillK@prodim-int.local>
2017-10-23 15:41:17 +0200

    Fucking easy solution for beautiful path

--- algorithms/graphs/python/beautifulPath.py
+++ algorithms/graphs/python/beautifulPath.py
@@ -2,29 +2,32 @@
 
 import sys
 from collections import deque
+from collections import Counter
 
-def findCost(start, end, graph, cost):
+def bfs(start, end, graph, exclude):
   queue = deque([start])
-    
+  checked = set([])
   while queue:
     current = queue.popleft()
-
+    if current == end:
+      return True
+    checked.add(current)
     for n in graph[current]:
-      v = n['v']
-      w = n['w']
-      if v == current:
+      v, w = n['v'], n['w']
+      if w | exclude > exclude:
         continue
-      needAdd = False
-      for c in cost[current]:
-        if c | w not in cost[v]:
-          cost[v].add(c | w)
-          needAdd = True
-       if needAdd and v not in queue:
-         queue.append(v)
+      if not v in checked:
+        queue.append(v)
+  return False
+    
 
-  if not cost[end]:
-    return -1
-  return min(cost[end])
+def findCost(start, end, graph, cost):
+  i = 0
+  while not bfs(start, end, graph, i):
+    i += 1
+    if i > 1023:
+      return -1
+  return i
   
 if __name__ == '__main__':
   line = input().split()

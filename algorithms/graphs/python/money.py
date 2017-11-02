d3781bd6632fb8eecc5e6c5d36295f3333bf558f
Kirill Kondraschoff <KirillK@prodim-int.local>
2017-11-02 09:29:33 +0100

    new edition of get money

--- algorithms/graphs/python/money.py
+++ algorithms/graphs/python/money.py
@@ -1,38 +1,63 @@
 #! env python
 
 from collections import Counter
-import copy
 
-def excludeVertex(vertex, graph):
-  result = copy.deepcopy(graph)
-  toDelete = result[vertex]['adjanced']
-  toDelete.add(vertex)
-  for v in toDelete:
-    del result[v]
-  for v in result:
-    for d in toDelete:
-      if d in result[v]['adjanced']:
-        result[v]['adjanced'].remove(d)
-  return result 
-
-def collectMoney(graph):
+def getCost(graph, mask):
   result = 0
   for v in graph:
-    node = graph[v]
-    residentResult, _ = collectMoney(excludeVertex(v, graph))
-    alt = node['c'] + residentResult
-    result = max(alt, result)
-  return result, 0
+    if 1 << v & mask:
+      result += graph[v]['c']
+  return result
+
+def collectMoney(graph, counter):
+  result = 0
+  for v in graph:
+    adjanced = graph[v]['adjanced']
+    disconnected = []
+    connected = []
+    for path in counter:
+      if not path & adjanced:
+        disconnected.append(path)
+      else:
+        connected.append(path)
+    if not connected:
+      counter[1 << v] = graph[v]['c']
+    for c in connected:
+      mask = 1 << v | c & ~adjanced
+      counter[mask] = getCost(graph, mask)
+    for d in disconnected:
+      mask = d | 1 << v
+      counter[mask] = getCost(graph, mask)
+
+
+def collectMoney1(graph, counter, passed = 0):
+  result = 0
+  for v in graph:
+    if not (1 << v) & passed:
+      node = graph[v]
+      newPassed = passed | (1 << v) | node['adjanced']
+      if newPassed not in counter:
+        residentResult = collectMoney1(graph, counter, newPassed)
+        alt = node['c'] + residentResult
+        result = max(alt, result)
+        counter[newPassed] = result
+  return result
 
 if __name__ == '__main__':
   nHouses, nRoads = [int(x) for x in input().split()]
   graph = {}
   money = [int(x) for x in input().split()]
   for i in range(1, nHouses + 1):
-    graph[i] = {'c': money[i - 1], 'adjanced': set([])}
+    graph[i] = {'c': money[i - 1], 'adjanced': 0}
   for i in range(nRoads):
     a, b = [int(x) for x in input().split()]
-    graph[a]['adjanced'].add(b)
-    graph[b]['adjanced'].add(a)
-  print(collectMoney(graph))
+    graph[a]['adjanced'] |= 1 << b;
+    graph[b]['adjanced'] |= 1 << a;
+  counter = Counter()
+  collectMoney(graph, counter)
+  #for c in sorted(counter):
+  #  print('%s %d' % (bin(c >> 1), counter[c]))
+  values = list(counter.values())
+  result = max(values)
+  print(result, values.count(result))
   #print('%d %d' % collectMoney(graph))
